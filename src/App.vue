23f6e081312ecfdecbe6b83f1d5e1ca846509ba4
zhongyu <931564975@qq.com>
2020-06-07 16:57:40 +0800

    修改页面模板

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,99 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: zhongyu
+ * @Date: 2020-06-06 20:27:16
+ * @LastEditors: zhongyu
+ * @LastEditTime: 2020-06-07 16:57:06
+--> 
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div id="app" class="app">
+        <el-container>
+            <el-aside :width="isCollapse ? '66px' : '200px'">
+                <div class="menu-title">
+                    前段VUE的DOME
+                </div>
+                <el-menu :default-active="this.defaultActive" class="el-menu-vertical-demo" :collapse="isCollapse" :style="isCollapse? 'width: 64px' : 'width: auto'">
+                    <router-link to="/home" class="menu-item-link">
+                        <el-menu-item index="home">
+                            <i class="el-icon-s-home"></i>
+                            <span slot="title">前往HOME页面</span>
+                        </el-menu-item>
+                    </router-link>
+                    <router-link to="/user" class="menu-item-link">
+                        <el-menu-item index="user">
+                            <i class="el-icon-user-solid"></i>
+                            <span slot="title">前往USER页面</span>
+                        </el-menu-item>
+                    </router-link>
+                </el-menu>
+            </el-aside>
+
+            <el-container>
+                <el-header>
+                    <div class="header-left">
+                        <div v-if="!isCollapse" @click="onCollapse()">
+                            <i class="collapse-icon el-icon-s-unfold"></i>
+                        </div>
+                        <div v-else @click="onCollapse()">
+                            <i class="collapse-icon el-icon-s-fold"></i>
+                        </div>
+                    </div>
+                    <div class="header-right">
+                        <i class="collapse-icon el-icon-setting" style="font-size: 20px"></i>
+                    </div>
+                </el-header>
+
+                <el-main>
+                    <router-view></router-view>
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script>
+
+import './app.scss';
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'app',
+    data() {
+        return {
+            isCollapse: false,
+            defaultActive: "home"
+        }
+    },
+    mounted() {
+        this.defaultActive = this.$router.history.current.name;
+        // console.log()
+    },
+    methods: {
+        onCollapse() {
+            this.isCollapse = !this.isCollapse;
+        },
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
+        }
     }
-  }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+// .el-header {
+//     background-color: #b3c0d1;
+//     color: #333;
+//     line-height: 60px;
+// }
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+// .el-aside {
+//     color: #333;
+// }
+// .el-menu-vertical-demo:not(.el-menu--collapse) {
+//     width: 200px;
+//     min-height: 400px;
+// }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
+import Router from 'vue-router';
+import routes from './router';
+
+Vue.use(Router);
+Vue.use(ElementUI);
+
+const router = new Router({
+    routes  // 注意routes 不可以写错
+});
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 })
